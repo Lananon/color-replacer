bad0f1fd086117077b63c178373926c3d7f7c3b4
Lana <lananon1807@gmail.com>
2026-01-30 15:46:01 +0100

    fix case sensitivity

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ def main():
     replacer_json= open(argv[2]).read()
     
     replacer_dict = json.loads(replacer_json)
-
+    
+    
     for key in replacer_dict:
+        file = file.replace(key.lower(), key.upper())
+        file = file.replace(key.upper(), key)
+        
         file = file.replace(key, replacer_dict[key])
 
     print(file)
